e2795b3331a7effb3aa4cf059db788301cf6f0e8
MIRACLE <31395674+engr-miraculous@users.noreply.github.com>
2019-12-21 23:55:23 +0100

    initial commit

---
+++ precache-manifest.931430d728f1b076551a37cecc391db1.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c383af4692f1422a5b",
+    "url": "/css/app.b41b63b9.css"
+  },
+  {
+    "revision": "e8c383af4692f1422a5b",
+    "url": "/js/app.99cde648.js"
+  },
+  {
+    "revision": "c7e46efd4afdee98d17a",
+    "url": "/css/chunk-vendors.a9104a14.css"
+  },
+  {
+    "revision": "c7e46efd4afdee98d17a",
+    "url": "/js/chunk-vendors.9e246adc.js"
+  },
+  {
+    "revision": "f6145fb2883e4c18c1da554e7f01d723",
+    "url": "/fonts/RopaSans-Regular.f6145fb2.ttf"
+  },
+  {
+    "revision": "7d4fa2c6bb6c7141b44d44ff86b9d56f",
+    "url": "/img/face-9.7d4fa2c6.jpeg"
+  },
+  {
+    "revision": "36e968cdbe86f6123a8831a9dd13c1aa",
+    "url": "/img/ultra_main.36e968cd.png"
+  },
+  {
+    "revision": "76c2876f27cc2eaa345eac0480939d63",
+    "url": "/img/35.76c2876f.gif"
+  },
+  {
+    "revision": "5f5566f014f1943730d8bd8108e2035b",
+    "url": "/img/sonoall.5f5566f0.png"
+  },
+  {
+    "revision": "942d3dd24937752526516d64e11a974e",
+    "url": "/img/health_bg2_pattern.942d3dd2.png"
+  },
+  {
+    "revision": "052fe6a857c9a9b05830b9ecb671fbc3",
+    "url": "/img/partner.052fe6a8.png"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/fonts/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/fonts/fa-brands-400d41d.9b6c8da3.eot"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/fonts/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/fonts/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/fonts/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/fonts/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/fonts/fa-regular-400d41d.7422060c.eot"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/fonts/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/fonts/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/fonts/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/fonts/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/fonts/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/fonts/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/fonts/fa-solid-900d41d.70e65a7d.eot"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/fonts/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/img/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/img/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/img/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "6ef50e83db39ca72b4c7813563c73505",
+    "url": "/img/patient.6ef50e83.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "861720fa94b80958075f620b2c6e8fb9",
+    "url": "/fonts/nucleo-icons.861720fa.woff"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "5d9a7f2513b7eeb13aad22884e9c1b2d",
+    "url": "/img/sonocare.5d9a7f25.png"
+  },
+  {
+    "revision": "844d1b01b0c2b39e63a6d9f8b90aaae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
